3902dbffa5e48c6ee72b1faf6ad5de929d3494f7
张亚飞 <672233580@qq.com>
2021-06-11 20:10:45 +0800

    Initial Commit

---
+++ Specs/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Specs
+//
+//  Created by 张亚飞 on 2021/6/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Specs/SpecsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpecsApp.swift
+//  Specs
+//
+//  Created by 张亚飞 on 2021/6/11.
+//
+
+import SwiftUI
+
+@main
+struct SpecsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
